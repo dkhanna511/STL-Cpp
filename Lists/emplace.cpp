1c36c77807312d0b3c9aecb283ea8013ad3f1550
dkhanna511 <khannaaiig@gmail.com>
2022-08-28 20:01:00 +0530

    Completed list functions

---
+++ Lists/emplace.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main()
+{
+    list<int> lis = {1,2,3,4,5,6};
+
+    auto it = lis.emplace(lis.begin(), 2);
+    
+    //inserts at the beginning of the list
+    lis.emplace(it, 1);
+
+    cout<<"List : "<<endl;
+    for(auto it = lis.begin(); it!=lis.end(); it++)
+    {
+        cout<<*it<<"  ";
+    }
+    return 0;
+
+    
+}
